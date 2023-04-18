495739d22d786804c77be33be835889f933c1ee5
mzokovvladimir <105171108+mzokovvladimir@users.noreply.github.com>
2023-04-18 14:58:15 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -7,6 +7,9 @@ class Post(models.Model):
     body = models.TextField(blank=True, default='')
     author = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         ordering = ['created']
 
@@ -17,6 +20,9 @@ class Comment(models.Model):
     author = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
     post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.post
+
     class Meta:
         ordering = ['created']
 
@@ -25,6 +31,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50, blank=False, default='')
     author = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
     posts = models.ManyToManyField('Post', related_name='categories', blank=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name_plural = 'categories'
